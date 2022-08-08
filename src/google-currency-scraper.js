fcad7b28e4fffbdb1c0131ccdc94eb2a35817eaa
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-08-08 22:30:16 +0300

    Add src/google-currency-scraper.js

--- src/google-currency-scraper.js
+++ src/google-currency-scraper.js
@@ -1,7 +1,75 @@
-const googleCurrencyScraper = () => {
-    // TODO
+// TODO: Make *'s in JSDoc better where possible
+import { closeBrowser, ensurePageLoadOnlyDocument, launchBrowser, openNewPage } from "./utils/browser.js";
+import { CurrencyCode, isValidCurrencyCode } from "./utils/currency-code.js";
+import { objectToQueryString } from "./utils/object-to-query-string.js";
+
+/**
+ * @param {object} options
+ * @param {CurrencyCode} options.from
+ * @param {CurrencyCode} options.to
+ * @returns {Promise<{rate: number, from: CurrencyCode, to: CurrencyCode}>}
+ */
+const googleCurrencyScraper = async ({ from, to }) => {
+    if (!isValidCurrencyCode(from)) {
+        throw new Error(`Invalid from currency code: ${from}`);
+    }
+
+    if (!isValidCurrencyCode(to)) {
+        throw new Error(`Invalid to currency code: ${to}`);
+    }
+
+    if (from === to) {
+        return {
+            rate: 1,
+            from,
+            to
+        };
+    }
+
+    const browser = await launchBrowser();
+
+    const page = await openNewPage(browser);
+
+    await ensurePageLoadOnlyDocument(page);
+
+    await goToGoogleCurrencySearchResult(page, from, to);
+
+    const exchangeRate = await parseExchangeRate(page);
+
+    await closeBrowser(browser);
+
+    return {
+        from,
+        to,
+        rate: exchangeRate
+    };
 };
 
-export {
-    googleCurrencyScraper
-};
+/**
+ * @param {*} page
+ * @param {CurrencyCode} from
+ * @param {CurrencyCode} to
+ * @returns {Promise<* | null>}
+ */
+async function goToGoogleCurrencySearchResult(page, from, to) {
+    const qs = objectToQueryString({
+        q: `1+${from}+to+${to}`,
+        hl: "en" // Make sure to use the English language to avoid any weirdness
+    });
+    return await page.goto(`https://www.google.com/search?${qs}`, {
+        waitUntil: "networkidle2"
+    });
+}
+
+/**
+ * @param {*} page
+ * @returns {Promise<number>}
+ */
+async function parseExchangeRate(page) {
+    return page.$eval(
+        "[data-exchange-rate]",
+        element => parseFloat(element.getAttribute("data-exchange-rate"))
+    );
+}
+
+export default googleCurrencyScraper;
