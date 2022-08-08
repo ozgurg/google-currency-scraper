1a532889e157ef2d093727067e90f38d3223c8b3
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-08-09 02:46:55 +0300

    Update currency-code.test.js

--- src/utils/currency-code.test.js
+++ src/utils/currency-code.test.js
@@ -1,11 +1,25 @@
-import { isValidCurrencyCode } from "./currency-code.js";
+import { CurrencyCode, isValidCurrencyCode } from "./currency-code.js";
 
 describe("utils/currency-code", () => {
-    it("should return 'true' for valid currency code", () => {
-        expect(isValidCurrencyCode("TRY")).toBe(true);
+    describe("CurrencyCode", () => {
+        it("should be an object", () => {
+            expect(typeof CurrencyCode).toBe("object");
+        });
+
+        it("should same key value pair", () => {
+            for (const [key, value] of Object.entries(CurrencyCode)) {
+                expect(key).toBe(value);
+            }
+        });
     });
 
-    it("should return 'false' for invalid currency code", () => {
-        expect(isValidCurrencyCode("ABC")).toBe(false);
+    describe("isValidCurrencyCode", () => {
+        it("should return 'true' for valid currency code", () => {
+            expect(isValidCurrencyCode("TRY")).toBe(true);
+        });
+
+        it("should return 'false' for invalid currency code", () => {
+            expect(isValidCurrencyCode("ABC")).toBe(false);
+        });
     });
 });
