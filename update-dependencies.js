0eb089273b16a8f3949f683ffaddaaea65633860
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-08-10 00:45:35 +0300

    Update dependencies

---
+++ update-dependencies.js
@@ -0,0 +1,19 @@
+import { exec } from "child_process";
+
+import packageJson from "./package.json" assert { type: 'json' };
+
+if(packageJson.dependencies !== undefined) {
+    const dependencies = Object.keys(packageJson.dependencies);
+
+    (async () => {
+        await exec(`npm i ${dependencies.join(" ")} --save`);
+    })();
+}
+
+if(packageJson.devDependencies !== undefined) {
+    const devDependencies = Object.keys(packageJson.devDependencies);
+
+    (async () => {
+        await exec(`npm i ${devDependencies.join(" ")} --save-dev`);
+    })();
+}
