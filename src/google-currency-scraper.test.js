230bb81bf125535a6e45b6f1f4ed34f02e7223b1
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-08-10 00:33:50 +0300

    Add google-currency-scraper.test.js

--- src/google-currency-scraper.test.js
+++ src/google-currency-scraper.test.js
@@ -1,5 +1,47 @@
+import googleCurrencyScraper from "./google-currency-scraper.js";
+
 describe("google-currency-scraper", () => {
-    it("placeholder", () => {
-        // Its placeholder
+    it("should throw an error if 'from' is invalid", async () => {
+        await expect(async () => {
+            await googleCurrencyScraper({
+                from: "A"
+            });
+        }).rejects.toThrow("Invalid 'from' currency code: A");
+    });
+
+    it("should throw an error if 'from' is invalid", async () => {
+        await expect(async () => {
+            await googleCurrencyScraper({
+                from: "TRY",
+                to: "A"
+            });
+        }).rejects.toThrow("Invalid 'to' currency code: A");
+    });
+
+    it("should return '1' rate without scraping Google if 'from' and 'to' are the same", async () => {
+        const scraper = await googleCurrencyScraper({
+            from: "USD",
+            to: "USD"
+        });
+        expect(scraper).toStrictEqual({
+            from: "USD",
+            to: "USD",
+            rate: 1
+        });
+
+        // TODO: Make sure the browser is not launched
+    });
+
+    it("should scrape Google and return valid result", async () => {
+        // I'm not sure about making a real scraping,
+        // but I think it will help me with the changes
+        // Google can make to its HTML structure in the future
+        const currency = await googleCurrencyScraper({
+            from: "TRY",
+            to: "USD"
+        });
+        expect(currency.from).toBe("TRY");
+        expect(currency.to).toBe("USD");
+        expect(typeof currency.rate).toBe("number");
     });
 });
