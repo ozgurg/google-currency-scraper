194ef55cddc4518459193efb3daf066cdc8d1030
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2024-05-19 21:02:57 +0300

    revert!: replace cheerio with regex
    BREAKING CHANGE: This change also changes the return value of dateUpdated

--- src/utils/date.js
+++ src/utils/date.js
@@ -4,33 +4,6 @@
  */
 const cleanDateInSearchResult = dateString => dateString.replace(" · ", "");
 
-/**
- * Parse dates formatted as "MMM DD, HH:mm UTC" in search results into the Date class.
- *
- * @param {string} dateString
- * @return {Date}
- */
-const parseAndNormalizeDateInSearchResult = dateString => {
-    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
-    const parts = dateString.split(" ");
-
-    const month = months.indexOf(parts[0]);
-    const day = parseInt(parts[1]);
-    const [hour, minute] = parts[2].split(":").map(part => parseInt(part));
-
-    const date = new Date();
-    date.setUTCMonth(month);
-    date.setUTCDate(day);
-    date.setUTCHours(hour);
-    date.setUTCMinutes(minute);
-    date.setUTCSeconds(0);
-    date.setUTCMilliseconds(0);
-
-    return date;
-};
-
 export {
-    cleanDateInSearchResult,
-    parseAndNormalizeDateInSearchResult
+    cleanDateInSearchResult
 };
--- src/utils/date.test.js
+++ src/utils/date.test.js
@@ -1,4 +1,4 @@
-import { cleanDateInSearchResult, parseAndNormalizeDateInSearchResult } from "./date.js";
+import { cleanDateInSearchResult } from "./date.js";
 import { describe, expect, it } from "vitest";
 
 describe("utils/date", () => {
@@ -6,13 +6,6 @@ describe("utils/date", () => {
         it("should clean date in search result", () => {
             const dateString = "Sep 01, 21:15 UTC · ";
             expect(cleanDateInSearchResult(dateString)).toBe("Sep 01, 21:15 UTC");
-        });
-    });
-
-    describe("parseAndNormalizeDateInSearchResult", () => {
-        it("should parse and normalize date in search result", () => {
-            const dateString = "Sep 01, 21:15 UTC";
-            expect(parseAndNormalizeDateInSearchResult(dateString)).toStrictEqual(new Date("2024-09-01T21:15:00.000Z"));
         });
     });
 });
