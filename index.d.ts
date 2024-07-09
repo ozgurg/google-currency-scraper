b68ed60856cc31b470580c55260a31adc64f5038
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2024-07-09 22:08:56 +0300

    feat: add TypeScript declarations

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+export default googleCurrencyScraper;
+export { CurrencyCode };
+import googleCurrencyScraper from "./src/google-currency-scraper.js";
+import { CurrencyCode } from "./src/utils/currency-code.js";
